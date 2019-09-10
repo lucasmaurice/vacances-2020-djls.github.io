10d15ea895cdc0fdf1681cbe78d5fc8d492766c1
Lucas <lucas.maurice@outlook.com>
2019-09-09 20:24:46 -0400

    Updated header.

--- js/main.js
+++ js/main.js
@@ -225,7 +225,7 @@ $(document).ready(function(){
 
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Sep 5, 2019 15:37:25").getTime();
+    var countDownDate = new Date("Dec 19, 2019 10:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
@@ -243,7 +243,7 @@ $(document).ready(function(){
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
         
         // Output the result in an element with id="demo"
-        document.getElementById("timer").innerHTML ="<div class='start-in'>start in:</div>"+ days + "<span>days  </span>: " + hours + "<span>hour</span>: "
+        document.getElementById("timer").innerHTML ="<div class='start-in'>Dans </div>"+ days + "<span>jours  </span>: " + hours + "<span>Heures</span>: "
         + minutes + "<span>mins  </span>: " + seconds + "<span>secs  </span>";
         
         // If the count down is over, write some text 
